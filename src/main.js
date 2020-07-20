6a3e9319d54c22472c34ff370537c1d0b39e0434
itaibn9 <itaibn9@gmail.com>
2020-07-20 17:07:01 +0300

    final style before adding bonuses

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 
 function add_todo_trigger(){
+    let inputValue = input.value;
+    if(inputValue === ""){
+        alert("to do text required");
+        return;
+    }
+    input.value = '';
     todoCounter ++;
     counter.textContent = todoCounter; // process of the counter of the todo tasks.
     //getting the data from the field.
-    let inputValue = input.value;
-    input.value = '';
+    if(todoCounter > limitTask)//feature reminder to complete your todo tasks;
+    {
+    alert("Come on start completing your tasks");
+    }
+    
+    
     let creationTime = new Date().toISOString().substr(0,18);
-    let timePresentor = creationTime.replace("T", " Created At: ")
+    let timePresentor = creationTime.replace("T", "  ")
      const selectedPriority = inputPriority.value;
      let dateTodo = dueDateInput.value;
      dueDateInput.value = '';
@@ -30,16 +40,23 @@ function show_task_on_screen(todo_priority, time, text, date){
     task.setAttribute('class', 'todoText');
     const timeCreated = document.createElement("span");
     timeCreated.setAttribute('class', 'todoCreatedAt');
-    timeCreated.textContent =" " + time + " - ";
+    timeCreated.textContent =" - Created At: " + time + " - ";
     const priority = document.createElement('strong');
     priority.setAttribute('class', 'todoPriority');
     priority.textContent = todo_priority;
     const dateExpire = document.createElement("strong");
     dateExpire.setAttribute('class', 'experassion_day');
-    if(date === ''){dateExpire.textContent = "No due time"}
-    else{dateExpire.textContent = " until: " + date;}
+    let today = new Date().toISOString().substr(0, 10);
+    if(today === date){
+        task.style.backgroundColor="yellow";
+        task.style.color="black";}
+        else if(today > date && date != ""){
+            task.style.backgroundColor="red";
+            task.style.color="black";}
+    if(date == ""){
+        dateExpire.textContent = "";}
+    else{dateExpire.textContent = " - until: " + date;}
     tasksList.appendChild(container);
-    if(text === "" || text === " " || text === "  "){text = "you didn't insert a Todo task"};
     task.textContent = text;
     doneButton.textContent = 'Done';
     container.appendChild(priority);
@@ -53,11 +70,7 @@ function show_task_on_screen(todo_priority, time, text, date){
     container.appendChild(anotherBreakLine);
     doneButton.addEventListener('click', function(){done_button_trigger(container, time);});
     input.focus(); // getting back to focus on the input.
-    if(todoCounter > 5)//feature reminder to complete your todo tasks;
-    {
-    alert("Come on start completing your tasks");
-    }
-   
+    
 }
 
 function done_button_trigger(childPlacement, deleted_todo_time){
@@ -86,7 +99,9 @@ function clean_presented_list() {
         tasksList.removeChild(tasksList.lastChild); // first it cleans the shown list;
     }
 }
+
 // control section:
+const limitTask = 5;
 const counter = document.getElementById("counter");
 let todoCounter = 0;
 counter.textContent = todoCounter; 
@@ -99,9 +114,10 @@ const input = document.querySelector('input');
 input.focus();
 const dueDateInput = document.getElementById("dueTime");
 addButton.addEventListener('click', add_todo_trigger);
-
-// sorting button and function.
+// sorting button and calling function.
 const sortButton = document.getElementById("sortButton");
 sortButton.addEventListener('click', sortButtonTrigger);
 
+let today = new Date().toISOString().substr(0, 10);
 
+document.querySelector("#dueTime").setAttribute('min', today);
